e237ca74bff0c73476bd5056d927a4a80adbf786
maxx6956 <max.rapp01@gmail.com>
2022-06-12 20:27:27 +0200

    laptop

--- network/models.py
+++ network/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Posts")
+    text = models.CharField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    like = models.ManyToManyField(User, related_name="Likes")
+    def __str__(self):
+        return str(f"{self.poster} made a post @ {self.timestamp}")
+
+class Follow(models.Model):
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
+    follow = models.ManyToManyField(User, null=True, blank=True, related_name="Follows")
+    def __str__(self):
+        return str(self.account)
--- network/views.py
+++ network/views.py
@@ -3,13 +3,87 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
-from .models import User
+from functools import reduce
+from operator import and_, or_
+
+from .models import User, Post, Follow
+
+
+class PostForm(forms.Form):
+    text = forms.CharField(max_length=288, label="",
+    widget=forms.Textarea(attrs={'class':'form-control w-100', 'cols': '150', 'rows': '5'}))
 
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            print(user)
+            form = form.cleaned_data
+            post = Post(poster = user, text = form["text"])
+            post.save()
+    posts = Post.objects.all()
+    posts = posts.order_by('-timestamp')
 
+    p = Paginator(posts, 10)
+    page = request.GET.get('page')
+    posts = p.get_page(page)
+
+    return render(request, "network/index.html", {
+        "form": PostForm,
+        "posts": posts
+    })
+
+
+def userpage(request, userpage):
+    user = User.objects.get(username = userpage)
+    follows = Follow.objects.get(account = user)
+    posts = user.Posts.all()
+
+    posts = posts.order_by('-timestamp')
+
+    p = Paginator(posts, 10)
+    page = request.GET.get('page')
+    posts = p.get_page(page)
+
+    print(user.Follows.all())
+    if request.method == "POST":
+        print(request.POST)
+        if request.POST["button"] == "Follow":
+           follows.follow.add(request.user)
+        elif request.POST["button"] == "Unfollow":
+            follows.follow.remove(request.user)
+    if request.user in follows.follow.all():
+        button = "Unfollow"
+    else:
+        button = "Follow"
+    return render(request, "network/userpage.html", {
+        "follows": follows,
+        "posts": posts,
+        "current": user,
+        "button": button,
+    })
+
+
+@login_required
+def following(request):
+    follows = request.user.Follows.all()
+    posts = Post.objects.filter(reduce(or_, [Q(poster=follow.account) for follow in follows]))
+    posts = posts.order_by('-timestamp')
+
+    p = Paginator(posts, 10)
+    page = request.GET.get('page')
+    posts = p.get_page(page)
+
+    return render(request, "network/following.html", {
+        "posts": posts,
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -53,6 +127,8 @@ def register(request):
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
+            follow = Follow(account=user)
+            follow.save()
         except IntegrityError:
             return render(request, "network/register.html", {
                 "message": "Username already taken."
